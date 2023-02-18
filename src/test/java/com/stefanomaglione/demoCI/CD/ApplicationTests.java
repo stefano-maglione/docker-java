c7e039093872aab52ad612afd1904d710fac2954
felpone <maglionestefanoinfo@gmail.com>
2023-02-18 22:26:23 +0100

    commit

--- src/test/java/com/stefanomaglione/demoCI/CD/ApplicationTests.java
+++ src/test/java/com/stefanomaglione/demoCI/CD/ApplicationTests.java
@@ -5,9 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testNegative() {
+		assert (false);
 	}
 
 }
